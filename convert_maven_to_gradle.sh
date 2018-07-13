bde6fa308c95d7bfaa46647db177e962015858ab
Baptiste Mesta <baptiste.mesta@gmail.com>
2018-07-13 09:18:53 +0200

    use patch instead of commits

--- convert_maven_to_gradle.sh
+++ convert_maven_to_gradle.sh
@@ -3,30 +3,40 @@
 set -euxo pipefail
 
 GRADLE_VERSION=4.8.1
+BASEDIR=$(pwd)/$(dirname "$0")
+
+
+#
+#  Create patches with
+#    git format-patch --stdout -1 HEAD > current_commit.patch
+#
+#  Apply with 
+#    git am -3 --ignore-whitespace <$BASEDIR/current_commit.patch
+#
 
 cd community
 echo "Community:"
-git cherry-pick 58cffacaa69c793ed0cd08dca62874c264184293 # activate tests + as-tests profiles
-git cherry-pick 409abc2c26269f5f66c8d0f38629b195e599b6b1 # .gitignore
+git am -3 --ignore-whitespace < $BASEDIR/58cffacaa69c793ed0cd08dca62874c264184293.patch # activate tests + as-tests profiles
+git am -3 --ignore-whitespace < $BASEDIR/409abc2c26269f5f66c8d0f38629b195e599b6b1.patch # .gitignore
 gradle init && git add --all && git commit -m "Gradle init - convert from Maven"
 echo "sed to replace providedCompile par compileOnly"
 find . -name build.gradle | xargs sed -i 's/providedCompile/compileOnly/g' && git commit -a -m "replace providedCompile par compileOnly"
 # echo "replace gradle wrapper version to 4.8.1 (or newer)"
 # sed -i 's@services.gradle.org/distributions/.*$@services.gradle.org/distributions/gradle-4.8.1-all.zip@g' gradle/wrapper/gradle-wrapper.properties && git commit -a -m "Use latest Gradle wrapper version"
 echo "XSD generation"
-git cherry-pick 9e4bb1ab215a97a48632589f7128b19ce9e7d061 # xsd generation
-git cherry-pick 7f3f57de9e389f589bc383654422f84a3cf7cec4 # version.properties file generation
-git cherry-pick 5a4b586048fc254ce48bb7b7c88b912807fc20b1 # fix various tests
-git cherry-pick 7fa5f7eb3e0152b480a8ea445497f18894ec1cb7 # run integration test in a specific task
-git cherry-pick bab1e9917c4f355c268fe2fe73bec66ca2cf9b4e # fix business data module dependnecies
-git cherry-pick 11162e273a87ed8d30f1b98a1e9a4de2bc0c604d # fix hibernate tests
-git cherry-pick 34d4b6fadfc5d93978eb1957119959e08c6db8c3 # fix bdm tests
-git cherry-pick 08c5c97bfe7dcfe83c21b575676fdaca18cbc336 # compile security scripts
-git cherry-pick 0b4c4616470f18fcea15e132d8960d41e8a763f7 # ignore test shade
-git cherry-pick b170919763d65529d37d509ed3fcf08da82ce46b # fix ejb tests
-git cherry-pick 1b30f9d4042acfc1bab8d2242ab690c7c3c5d9e1 # fix http api test
-git cherry-pick f3c3e992cb7dfab30f8ca9519c4259744ac74373 # restore platform-setup zip file (previously from Assembly)
-git cherry-pick ea7ca78a2497fef70e72bf0978808d42a5671bab # javadoc(BOS): generate javadoc for all Community
+git am -3 --ignore-whitespace < $BASEDIR/9e4bb1ab215a97a48632589f7128b19ce9e7d061.patch # xsd generation
+git am -3 --ignore-whitespace < $BASEDIR/7f3f57de9e389f589bc383654422f84a3cf7cec4.patch # version.properties file generation
+git am -3 --ignore-whitespace < $BASEDIR/5a4b586048fc254ce48bb7b7c88b912807fc20b1.patch # fix various tests
+git am -3 --ignore-whitespace < $BASEDIR/7fa5f7eb3e0152b480a8ea445497f18894ec1cb7.patch # run integration test in a specific task
+git am -3 --ignore-whitespace < $BASEDIR/bab1e9917c4f355c268fe2fe73bec66ca2cf9b4e.patch # fix business data module dependnecies
+git am -3 --ignore-whitespace < $BASEDIR/11162e273a87ed8d30f1b98a1e9a4de2bc0c604d.patch # fix hibernate tests
+git am -3 --ignore-whitespace < $BASEDIR/34d4b6fadfc5d93978eb1957119959e08c6db8c3.patch # fix bdm tests
+git am -3 --ignore-whitespace < $BASEDIR/08c5c97bfe7dcfe83c21b575676fdaca18cbc336.patch # compile security scripts
+git am -3 --ignore-whitespace < $BASEDIR/0b4c4616470f18fcea15e132d8960d41e8a763f7.patch # ignore test shade
+git am -3 --ignore-whitespace < $BASEDIR/b170919763d65529d37d509ed3fcf08da82ce46b.patch # fix ejb tests
+git am -3 --ignore-whitespace < $BASEDIR/1b30f9d4042acfc1bab8d2242ab690c7c3c5d9e1.patch # fix http api test
+git am -3 --ignore-whitespace < $BASEDIR/f3c3e992cb7dfab30f8ca9519c4259744ac74373.patch # restore platform-setup zip file (previously from Assembly)
+git am -3 --ignore-whitespace < $BASEDIR/da31fdf4d9e1c229b9cdc6bd892c22f3365ec96a.patch # javadoc(BOS): generate javadoc for all Community
 
 find . -name "*.gradle" -type f | xargs sed -i "s/':bonita-platform/':platform/g"
 git commit -a -m "rename bonita-platform by platform"
@@ -36,12 +46,12 @@ sed -i '/rootProject/d' engine-settings.gradle
 find . -name "*.gradle" -type f | xargs sed -ri "s/':([^ ])/':engine:\1/g"
 git add --all &&  git commit -a -m "extract community settings gradle"
 
-git cherry-pick 09c0d50f8856f3f5e2004810cbd937e2bd89f3a0 # new settings.gradle
-git cherry-pick 7a3d7bdf12e8dda455679751e26e45c9e3ec0bfc # do not apply Java plugin to root project Community
+git am -3 --ignore-whitespace < $BASEDIR/09c0d50f8856f3f5e2004810cbd937e2bd89f3a0.patch # new settings.gradle
+git am -3 --ignore-whitespace < $BASEDIR/7a3d7bdf12e8dda455679751e26e45c9e3ec0bfc.patch # do not apply Java plugin to root project Community
 
 cd ..
 echo "Subscription:"
-git cherry-pick 0bea2555dd6e4bf902eaddbf9eb74fb3f384caa3 # activate as-tests and tests by default
+git am -3 --ignore-whitespace < $BASEDIR/0bea2555dd6e4bf902eaddbf9eb74fb3f384caa3.patch # activate as-tests and tests by default
 gradle init
 find . -name "build.gradle.bak" -exec rm {} \;
 git checkout -- community/
@@ -54,12 +64,8 @@ find subscription/ -name "*.gradle" -type f | xargs sed -i 's/:bonita-engine/:en
 find subscription/ -name "*.gradle" -type f | xargs sed -i 's/:engine:bonita-platform/:engine:platform/g'
 git add --all && git commit -m "Gradle init SP"
 
-git cherry-pick 3c63d54bdac3eacff0662aec8ab25cc62ea2b7b5 # Fix SP settings.gradle
+git am -3 --ignore-whitespace < $BASEDIR/3c63d54bdac3eacff0662aec8ab25cc62ea2b7b5.patch # Fix SP settings.gradle
 
 echo "sed to replace providedCompile par compileOnly"
 find subscription/ -name build.gradle | xargs sed -i 's/providedCompile/compileOnly/g' && git commit -a -m "replace providedCompile par compileOnly SP"
 sed -i "s/Compatibility = 1\.5/Compatibility = 1.8/g" build.gradle && git commit -a -m "Set Java source and target to 1.8"
-
-
-
-# git cherry-pick  #
