c9f629d5abe2a23dfd1a9f65bf208bc11ecc1435
Baptiste Mesta <baptiste.mesta@gmail.com>
2018-07-16 13:29:04 +0200

    fix getPlatform version

--- convert_maven_to_gradle.sh
+++ convert_maven_to_gradle.sh
@@ -84,3 +84,4 @@ git am -3 --ignore-whitespace < $BASEDIR/it_test_cluster.patch # it tests cluste
 git am -3 --ignore-whitespace < $BASEDIR/it_test_plugin.patch # extract license plugin
 git am -3 --ignore-whitespace < $BASEDIR/fix_it_compile.patch # some fix on integration tests
 git am -3 --ignore-whitespace < $BASEDIR/fix_distrib_it_sp.patch # distrib sp test fix
+git am -3 --ignore-whitespace < $BASEDIR/fix_getplaformversion.patch # get getPlatformVersion
