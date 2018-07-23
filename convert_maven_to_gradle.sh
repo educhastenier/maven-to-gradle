037b5cd3b8af66174e9291097b230b838bbb2f21
Baptiste Mesta <baptiste.mesta@gmail.com>
2018-07-23 13:35:55 +0200

    rename original jar in shade

--- convert_maven_to_gradle.sh
+++ convert_maven_to_gradle.sh
@@ -89,3 +89,4 @@ git am -3 --ignore-whitespace < $BASEDIR/test_memory.patch # test memory
 git am -3 --ignore-whitespace < $BASEDIR/utf-8.patch # test memory
 git am -3 --ignore-whitespace < $BASEDIR/cluster-test-flags.patch # cluster test jvm flags
 git am -3 --ignore-whitespace < $BASEDIR/shade.patch # shade
+git am -3 --ignore-whitespace < $BASEDIR/shade2.patch # rename original jar
