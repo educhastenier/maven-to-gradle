c5c046443b7aeb53f11b55d8a6cb565a7fa9125c
Baptiste Mesta <baptiste.mesta@gmail.com>
2018-07-24 11:35:54 +0200

    transitive deps on shade

--- convert_maven_to_gradle.sh
+++ convert_maven_to_gradle.sh
@@ -93,3 +93,4 @@ git am -3 --ignore-whitespace < $BASEDIR/shade2.patch # rename original jar
 git am -3 --ignore-whitespace < $BASEDIR/shade3.patch # shade external dependencies
 git am -3 --ignore-whitespace < $BASEDIR/shade-bdm-generator.patch # bdm generator shade
 git am -3 --ignore-whitespace < $BASEDIR/application_xsd.patch # remove schema2.xsd of application xsd gen
+git am -3 --ignore-whitespace < $BASEDIR/shade_transitive.patch # remove schema2.xsd of application xsd gen
